b8c6da4fb4f96bbba5f9a413406b9acf2d387e46
Akinyele Ebenezer <tayorakinyele6@gmail.com>
2022-09-08 18:40:06 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
- * */
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
+			puzzle");
 	return (0);
 }
 
