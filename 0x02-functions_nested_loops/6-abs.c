767bf457b223cab5a8a5395bb49011afb241a73e
Akinyele Ebenezer <tayorakinyele6@gmail.com>
2022-09-13 10:43:35 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
  * _abs - a function that computes the absolute value of an integer
  * @ab: integer input
  * Return: absolute value of ab
- * */
+ */
 int _abs(int ab)
 {
 	return (ab * ((ab > 0) - (ab < 0)));
