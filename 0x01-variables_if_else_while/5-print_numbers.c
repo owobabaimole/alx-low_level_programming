74290258051b5cb10efc3867842553dab18ac3ab
Akinyele Ebenezer <tayorakinyele6@gmail.com>
2022-09-12 21:14:57 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line, except q and e
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+while (n < 10)
+{
+	printf("%d", n);
+	n += 1;
+}
+printf("\n");
+return (0);
+}
