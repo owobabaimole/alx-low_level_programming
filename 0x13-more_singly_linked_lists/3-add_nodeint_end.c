1e84731c42b8077cdb49b946eeae3b5ced494a3b
Akinyele Ebenezer <tayorakinyele6@gmail.com>
2022-10-26 21:55:45 -0700

    fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,68 +1,40 @@
-
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_nodeint_end - Function that adds a node at the end
- * @head: the first element
- * @n: integer
- * Return: returns type of listint_t
+ * add_nodeint_end - add a new node at the end
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new, *temp;
+	listint_t *new;
+	listint_t *temp;
+
+	(void)temp;
 
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->next = NULL;
-
+	temp = *head;
 	if (*head == NULL)
 	{
-	*head = new;
+		*head = new;
 	}
 	else
 	{
-		temp = *head;
 		while (temp->next != NULL)
+		{
 			temp = temp->next;
+		}
 		temp->next = new;
 	}
+
 	return (*head);
-
-}#include <stdio.h>
-#include "lists.h"
-
-/**
- * add_nodeint_end - Function that adds a node at the end
- * @head: the first element
- * @n: integer
- * Return: returns type of listint_t
- */
-
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *new, *temp;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-	{
-	*head = new;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
-	return (*head);
-
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
  *
- * Description: Sets the head to NULL.
+ * Return: no return.
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *curr;
 
-	if (head == NULL)
-		return;
-
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
-
-	head = NULL;
 }
